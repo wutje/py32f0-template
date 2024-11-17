bd4bae604438e1f82de2b8b1617021fd1c55df99
Yuri Schaeffer <yuri@schaeffer.tk>
2024-11-17 22:38:15 +0100

    Get interrupts working

--- raddr/main.c
+++ raddr/main.c
@@ -1,11 +1,7 @@
-/***
- * Demo: LED Toggle
- *
- * PA0   ------> LED+
- * GND   ------> LED-
- */
-//#include "py32f0xx_bsp_printf.h"
+//PLAN: hook up switch to irq, toggle led
+
 #include "py32f0xx_bsp_clock.h"
+#include "py32f0xx_hal_exti.h"
 
 /*  A wolf is:
  *  PA1 = switch (Pin 7)
@@ -22,67 +18,57 @@
 #define KEY_IN_PIN  GPIO_PIN_3
 #define KEY_OUT_PIN GPIO_PIN_4
 
-static void APP_GPIO_Config(void);
+void cfg_pin(uint32_t pin, uint32_t mode, uint32_t pull)
+{
+    GPIO_InitTypeDef pin_cfg;
+
+    pin_cfg.Pin   = pin;
+    pin_cfg.Mode  = mode;
+    pin_cfg.Pull  = pull;
+    pin_cfg.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &pin_cfg);
+}
+
+static void cfg_gpio(void)
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    cfg_pin(SWC_PIN,     GPIO_MODE_IT_FALLING, GPIO_PULLUP);
+    cfg_pin(KEY_IN_PIN,  GPIO_MODE_INPUT,      GPIO_PULLUP);
+    cfg_pin(KEY_OUT_PIN, GPIO_MODE_OUTPUT_PP,  GPIO_NOPULL);
+
+    /* EXTI interrupt init*/
+    HAL_NVIC_SetPriority(EXTI0_1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+}
+
+void EXTI0_1_IRQHandler(void)
+{
+    GPIO_PinState switch_in;
+    switch_in = HAL_GPIO_ReadPin(GPIOA, SWC_PIN);
+    HAL_GPIO_WritePin(GPIOA, KEY_OUT_PIN, switch_in);
+}
+
 
 int main(void)
 {
     HAL_Init();
     BSP_HSI_24MHzClockConfig();
-    APP_GPIO_Config();
+    cfg_gpio();
 
     GPIO_PinState switch_in, key_in, key_out;
 
-    while (1)
-    {
-        // Inverteer alle inputs zodat ik mijn logica blijf snappen
-        switch_in = !HAL_GPIO_ReadPin(GPIOA, SWC_PIN);
-        key_in    = !HAL_GPIO_ReadPin(GPIOA, KEY_IN_PIN);
+    while (1);
+    if(0){
+        /*// Inverteer alle inputs zodat ik mijn logica blijf snappen*/
+        /*switch_in = !HAL_GPIO_ReadPin(GPIOA, SWC_PIN);*/
+        /*key_in    = !HAL_GPIO_ReadPin(GPIOA, KEY_IN_PIN);*/
 
-        key_out = switch_in | key_in;
+        /*[>key_out = switch_in | key_in;<]*/
+        /*key_out = switch_in;*/
 
-        // Inverteer alle outputs zodat ik mijn logica blijf snappen
-        // (ledje hangt tussen vcc en key_out)
-        HAL_GPIO_WritePin(GPIOA, KEY_OUT_PIN, !key_out);
+        /*// Inverteer alle outputs zodat ik mijn logica blijf snappen*/
+        /*// (ledje hangt tussen vcc en key_out)*/
+        /*HAL_GPIO_WritePin(GPIOA, KEY_OUT_PIN, !key_out);*/
     }
 }
-
-static void APP_GPIO_Config(void)
-{
-    GPIO_InitTypeDef cfg_key_out, cfg_key_in, cfg_switch;
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /*PA1, SWITCH*/
-    cfg_switch.Pin   = SWC_PIN;
-    cfg_switch.Mode  = GPIO_MODE_INPUT;
-    cfg_switch.Pull  = GPIO_PULLUP;
-    cfg_switch.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &cfg_switch);
-    /*PA3,  KEY IN*/
-    cfg_key_in.Pin   = KEY_IN_PIN;
-    cfg_key_in.Mode  = GPIO_MODE_INPUT;
-    cfg_key_in.Pull  = GPIO_PULLUP;
-    cfg_key_in.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &cfg_key_in);
-    /*PA4, KEY OUT*/
-    cfg_key_out.Pin   = KEY_OUT_PIN;
-    cfg_key_out.Mode  = GPIO_MODE_OUTPUT_PP;
-    cfg_key_out.Pull  = GPIO_PULLUP;
-    cfg_key_out.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &cfg_key_out);
-}
-
-void APP_ErrorHandler(void)
-{
-    while (1);
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Export assert error source and line number
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-    /* printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-    while (1);
-}
-#endif /* USE_FULL_ASSERT */
