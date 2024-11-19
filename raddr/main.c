6320c8c180ddc41ee2792a8276037acd79ee007b
Yuri Schaeffer <yuri@schaeffer.tk>
2024-11-19 23:10:09 +0100

    Almost working
    Problem: we can't sleep in an interrupt. Therefore we must either handle
    sending bits out of band or we must call the statemachine out of the
    interrupt.

--- raddr/main.c
+++ raddr/main.c
@@ -2,6 +2,9 @@
 #include <py32f0xx_hal_rcc.h>
 #include "clk_config.h"
 
+#include "wolf.h"
+#include "pack.h"
+
 /*  A wolf is:
  *  PA1 = switch (Pin 7)
  *  PA2 = shared with reset (Pin 6)
@@ -13,74 +16,124 @@
  *
  * */
 
-#define LED_PIN     GPIO_PIN_4
+#define SWC_PIN     GPIO_PIN_1
+#define KEY_IN_PIN  GPIO_PIN_3
+#define KEY_OUT_PIN GPIO_PIN_4
 
-static void APP_GPIO_Config(void);
+// if parent send dataframes continuously to trigger child
+#define PARENT 0
 
-static void do_debug_print(void) {
-    static int i = 0;
-    switch (i++) {
-        case 0: printf("Hello World\r\n"); break;
-        case 1: printf("Hello Yuri\r\n"); break;
-        case 2: printf("Holw\r\n"); break;
-        case 3: printf("Growl\r\n"); break;
-        case 4: printf("FULL MOON!\r\n"); break;
-        default:
-            i = 0;
-    }
+void cfg_pin(uint32_t pin, uint32_t mode, uint32_t pull)
+{
+    GPIO_InitTypeDef pin_cfg;
+
+    pin_cfg.Pin   = pin;
+    pin_cfg.Mode  = mode;
+    pin_cfg.Pull  = pull;
+    pin_cfg.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &pin_cfg);
 }
 
+static void cfg_gpio(void)
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    cfg_pin(SWC_PIN,     GPIO_MODE_IT_RISING_FALLING, GPIO_PULLUP);
+    cfg_pin(KEY_IN_PIN,  GPIO_MODE_IT_RISING, GPIO_PULLUP);
+    cfg_pin(KEY_OUT_PIN, GPIO_MODE_OUTPUT_PP,  GPIO_NOPULL);
+
+    /* EXTI interrupt init*/
+    HAL_NVIC_SetPriority(EXTI0_1_IRQn, 3, 0);
+    HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+
+    HAL_NVIC_SetPriority(EXTI2_3_IRQn, 3, 0);
+    HAL_NVIC_EnableIRQ(EXTI2_3_IRQn);
+}
+
+void gpio_set(int bit)
+{
+    HAL_GPIO_WritePin(GPIOA, KEY_OUT_PIN, bit);
+}
+
+void sleep_ns(int delay)
+{
+    HAL_Delay(delay); //TODO these are ms
+}
+
+int gpio_get(void)
+{
+    return HAL_GPIO_ReadPin(GPIOA, KEY_IN_PIN);
+}
+
+bool SOME_INPUT[K] = {0,0,0,0, 0,0,0,0};
+
+void update_input(void)
+{
+    SOME_INPUT[0] = !HAL_GPIO_ReadPin(GPIOA, SWC_PIN);
+}
+
+//switch
+void EXTI0_1_IRQHandler(void)
+{
+    //maybe read switch state here instead of update_input()?
+    __HAL_GPIO_EXTI_CLEAR_IT(SWC_PIN);
+}
+
+//key
+void EXTI2_3_IRQHandler(void)
+{
+    if (!PARENT) {
+        //trigger statemachine
+        receive_bit();
+        /*int a = HAL_GPIO_ReadPin(GPIOA, KEY_IN_PIN);*/
+        /*HAL_GPIO_WritePin(GPIOA, KEY_OUT_PIN, a);*/
+    }
+    __HAL_GPIO_EXTI_CLEAR_IT(KEY_IN_PIN);
+}
 
 int main(void)
 {
 #if defined USE_SEMIHOSTING
     {
-    //This 'magic' function needs to be called.
-    //libgloss will setup I/O structures to use
-    extern void initialise_monitor_handles();
-    initialise_monitor_handles();
+        //This 'magic' function needs to be called.
+        //libgloss will setup I/O structures to use
+        extern void initialise_monitor_handles();
+        initialise_monitor_handles();
     }
 #endif
 
-  HAL_Init();
-  BSP_HSI_24MHzClockConfig();
+    HAL_Init();
+    BSP_HSI_24MHzClockConfig();
+    cfg_gpio();
 
-  APP_GPIO_Config();
-  printf("PY32F0xx LED Toggle Demo\r\nSystem Clock: %ld\r\n", SystemCoreClock);
+    printf("PY32F0xx LED Toggle Demo\r\nSystem Clock: %ld\r\n", SystemCoreClock);
 
-  while (1)
-  {
-    HAL_Delay(1000);
-    HAL_GPIO_TogglePin(GPIOA, LED_PIN);
-    do_debug_print();
-  }
+    while (1) {
+        if (PARENT) {
+            statemachine(1);
+            HAL_Delay(T1L); //TODO these are ms
+            statemachine(1);
+            HAL_Delay(T1L); //TODO these are ms
+            HAL_Delay(1000); //TODO these are ms
+            printf("howl!\r\n");
+        }
+
+    }
 }
 
-static void APP_GPIO_Config(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct;
-
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  // PA0
-  GPIO_InitStruct.Pin = LED_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-}
 
 void APP_ErrorHandler(void)
 {
-  while (1);
+    while (1);
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Export assert error source and line number
-  */
+ * @brief  Export assert error source and line number
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  while (1);
+    /* printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    while (1);
 }
 #endif /* USE_FULL_ASSERT */
