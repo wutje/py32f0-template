68f4d26199b97d1ddc884d777816c48f28c0213a
Wouter van Gulik <wouter.van.gulik@mep-info.com>
2024-11-27 14:18:05 +0100

    output_timer.c: Do not use HAL.

--- raddr/output_timer.c
+++ raddr/output_timer.c
@@ -12,21 +12,22 @@
 // CLK  Divider     Single      Max
 // 24M  /1          41,66ns     2730ns
 // 12M  /2          83,33ns     5416ns
-//  8M  /3          125ns       8192ns
+//  8M  /3            125ns     8192ns
+//  1M  /24          1000ns     65535ms
 
-static TIM_HandleTypeDef TimHandle = {
-    /* TIM16, initial frequency = 24,000,000
-     * Prescale = 3 => 24 / 3 = 8,000,000 Mhz run rate
-     * Period = 8000 => 8Mhz / 8000 = 1Khz
-     * Then count to 8000 gives 1000 interrupts per second or 1Khz */
-    .Instance = TIM16,
-    .Init.Prescaler         = TIMER_DIVIDER - 1, // = 600
-    .Init.Period            = 40000, //1Hz
-    .Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1,
-    .Init.CounterMode       = TIM_COUNTERMODE_UP,
-    .Init.RepetitionCounter = 1 - 1,
-    .Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE,
-};
+//static const TIM_HandleTypeDef TimHandle = {
+//    /* TIM16, initial frequency = 24,000,000
+//     * Prescale = 3 => 24 / 3 = 8,000,000 Mhz run rate
+//     * Period = 8000 => 8Mhz / 8000 = 1Khz
+//     * Then count to 8000 gives 1000 interrupts per second or 1Khz */
+//    .Instance = TIM16,
+//    .Init.Prescaler         = TIMER_DIVIDER - 1, // = 600
+//    .Init.Period            = 40000, //1Hz
+//    .Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1,
+//    .Init.CounterMode       = TIM_COUNTERMODE_UP,
+//    .Init.RepetitionCounter = 1 - 1,
+//    .Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE,
+//};
 
 //TODO move to common header
 #define ARRAY_SIZE(a)  (sizeof(a)/sizeof(a[0]))
@@ -45,7 +46,7 @@ static struct {
     uint32_t buf[FIFO_SIZE];
 } fifo;
 
-static bool fifo_is_empty(void)
+bool fifo_is_empty(void)
 {
     return fifo.size == 0;
 }
@@ -60,7 +61,6 @@ static uint32_t fifo_read(void)
     uint32_t idx = fifo.read;
     uint32_t d = fifo.buf[idx];
     fifo.read = (idx + 1) % FIFO_SIZE;
-    fifo.size--;
     return d;
 }
 
@@ -75,15 +75,15 @@ void fifo_write(bool bit, uint16_t tmo)
 
     uint32_t d = tmo | bit << 31;
 
-    fifo.buf[fifo.write] = d;
-    fifo.write = (fifo.write + 1) % FIFO_SIZE;
-    fifo.size++;
-    /* If ISR not enabled the code is not running. Start it*/
-    if (__HAL_TIM_GET_IT_SOURCE(&TimHandle, TIM_IT_UPDATE) == RESET) {
-        /* Generate event to trigger this ISR */
+    fifo.buf[fifo.write % ARRAY_SIZE(fifo.buf)] = d;
+    fifo.write++;
+    /* If ISR not enabled the code is not running. Start it */
+    if (!(TIM16->DIER & TIM_DIER_UIE)) {
+        /* Generate event to RESET ALL counter values.
+         * This is need to get accurate timing for the first bit. */
         TIM16->EGR = TIM_EGR_UG;
-        __HAL_TIM_ENABLE_IT(&TimHandle, TIM_DIER_UIE);
-        //__HAL_TIM_ENABLE_IT(&TimHandle, TIM_IT_UPDATE);
+        /* Enable the timer interrupt */
+        TIM16->DIER = TIM_DIER_UIE;
     }
 }
 
@@ -102,28 +102,25 @@ void TIM16_IRQHandler(void)
      * That means no crappy bloated HAL code here!
      * Just old skool register writing */
 
-    if(fifo_is_empty()) {
-        //TODO Always off or on if no bit available? What is the 'rest' state?
-        GPIOA->BSRR = LED_PIN << 16;
-        //GPIOA->BRR = 16;
-        /* Disable the interrupt, to force not getting here again and signal to the writer we are done */
-        TIM16->DIER = 0;
-        return;
+    uint32_t size = fifo.size;
+    uint32_t t = 0;
+    if (size > 0) {
+        fifo.size = --size;
+        t = fifo_read();
     }
-    uint32_t t = fifo_read();
+
     uint32_t tmo = t; //MEGA hack, just write all 32 bit to ARR.
     uint32_t bit = !!(t & 1u<<31);
 
-    if (1) {
-        GPIOA->BSRR = bit ? LED_PIN : LED_PIN << 16;
-    }
-    else if (bit) {
-        /* BSRR => Bit Set Reset Register. Write 1 to set I/O */
+    /* BSRR => Bit Set Reset Register. Write 1 to set I/O */
+    if (bit)
         GPIOA->BSRR = LED_PIN;
-    } else {
-        /* BRR => Bit Reset Register. Write 1 to clear I/O */
-        GPIOA->BRR = LED_PIN;
-    }
+    else
+        GPIOA->BSRR = LED_PIN << 16;
+
+    /* Disable the interrupt, to force not getting here again and signal to the writer we are done */
+    if (tmo == 0)
+        TIM16->DIER = 0;
 
     /* Update reload register with new timeout */
     TIM16->ARR = tmo;
@@ -135,17 +132,33 @@ void TIM16_IRQHandler(void)
 
     /* Acknowledge the interrupt */
     TIM16->SR = ~TIM_IT_UPDATE;
+
 }
 
 void raddr_output_init(void)
 {
+    uint32_t tmpcr1;
+
     /* First enable the clocking towards TIM16 */
     __HAL_RCC_TIM16_CLK_ENABLE();
 
-    /* Setup timer and start.
-     * Note that we do NOT enable the timer internal interrupt just yet */
-    HAL_TIM_Base_Init(&TimHandle);
-    HAL_TIM_Base_Start_IT(&TimHandle);
+    /* Set the Autoreload value to max */
+    //TIM16->ARR = ~0;
+
+    /* Set the Prescaler value to the one calculated in the headerfile. */
+    TIM16->PSC = TIMER_DIVIDER;
+
+    /* Setup timer for simple upcounting */
+    tmpcr1 = 0;
+    tmpcr1 |= TIM_COUNTERMODE_UP; //0
+    tmpcr1 |= TIM_CLOCKDIVISION_DIV1; //0
+    //TODO do we actually need thisr? We no always reset using TIM_EGR_UG
+    tmpcr1 |= TIM_CR1_ARPE; //1 << 7
+
+    TIM16->CR1 = tmpcr1;
+
+    /* Enable the timer. */
+    TIM16->CR1 |= TIM_CR1_CEN;
 
     /* We need to be the highest priority, to ensure rock solid jitter free output! */
     HAL_NVIC_SetPriority(TIM16_IRQn, 0, 0);
